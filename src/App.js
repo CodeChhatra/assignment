98f211553f19335bcd62c04c44d03bca88c97eb0
code.chhatra <rameshdiwedi022@gmail.com>
2023-12-03 19:09:58 +0530

    Main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,156 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import { MdDelete, MdEdit } from 'react-icons/md';
 import './App.css';
+import Tablerow from './components/Tablerow';
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [filteredUsers, setFilteredUsers] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const itemsPerPage = 10;
+
+  useEffect(() => {
+    fetch('https://geektrust.s3-ap-southeast-1.amazonaws.com/adminui-problem/members.json')
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setFilteredUsers(data);
+      })
+      .catch((error) => {
+        console.error('Error fetching users:', error);
+      });
+  }, []);
+
+  useEffect(() => {
+    const filtered = users.filter((user) =>
+      Object.values(user).some((value) =>
+        value.toString().toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    );
+    setFilteredUsers(filtered);
+    setCurrentPage(1);
+  }, [searchTerm, users]);
+
+  const handlePageChange = (page) => {
+    setCurrentPage(page);
+  };
+
+  const pageNumbers = [];
+  const totalItems = filteredUsers.length;
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+
+  for (let i = 1; i <= totalPages; i++) {
+    pageNumbers.push(i);
+  }
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems = filteredUsers.slice(indexOfFirstItem, indexOfLastItem);
+
+  const handleRowSelect = (id) => {
+    const selectedIndex = selectedRows.indexOf(id);
+    let newSelectedRows = [...selectedRows];
+
+    if (selectedIndex === -1) {
+      newSelectedRows.push(id);
+    } else {
+      newSelectedRows.splice(selectedIndex, 1);
+    }
+    setSelectedRows(newSelectedRows);
+  };
+
+  const handleSelectAll = () => {
+    if (selectedRows.length === currentItems.length) {
+      setSelectedRows([]);
+    } else {
+      const allIds = currentItems.map((user) => user.id);
+      setSelectedRows(allIds);
+    }
+  };
+
+  const handleDeleteSelected = () => {
+    const updatedUsers = users.filter((user) => !selectedRows.includes(user.id));
+    setUsers(updatedUsers);
+    setSelectedRows([]);
+  };
+
+  const handleEditUser = (userId, field, value) => {
+    const updatedUsers = users.map((user) => {
+      if (user.id === userId) {
+        return {
+          ...user,
+          [field]: value,
+        };
+      }
+      return user;
+    });
+    setUsers(updatedUsers);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App p-6">
+      <div className="flex items-center mb-4">
+        <div className="relative w-full max-w-md">
+          <span className="absolute inset-y-0 left-0 pl-3 flex items-center">
+            {/* Search Icon */}
+          </span>
+          <input
+            type="text"
+            className="block w-full pl-10 pr-3 py-2 rounded-lg border-gray-300 focus:border-indigo-500 focus:ring focus:ring-indigo-200 focus:ring-opacity-50"
+            placeholder="Search..."
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+        </div>
+      </div>
+
+      <table className="min-w-full rounded-lg overflow-hidden shadow-lg">
+        <thead className="bg-gray-800 text-white">
+          <tr>
+            <th className="px-4 py-3">
+              <input
+                type="checkbox"
+                onChange={handleSelectAll}
+                checked={selectedRows.length === currentItems.length && currentItems.length > 0}
+
+              />
+            </th>
+            <th className="px-4 py-3">Name</th>
+            <th className="px-4 py-3">Email</th>
+            <th className="px-4 py-3">Role</th>
+            <th className="px-4 py-3">Actions</th>
+          </tr>
+        </thead>
+        <tbody className="bg-white">
+          {currentItems.map((user) => (
+            <Tablerow user={user} selectedRows ={selectedRows} handleRowSelect ={handleRowSelect} handleEditUser ={handleEditUser} handleDeleteSelected= {handleDeleteSelected} />
+          ))}
+        </tbody>
+      </table>
+
+      {/* Pagination */}
+      <div className="mt-4">
+        {pageNumbers.map((page) => (
+          <button
+            key={page}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
+            onClick={() => handlePageChange(page)}
+          >
+            {page}
+          </button>
+        ))}
+      </div>
+
+      {/* Delete selected button */}
+      <button
+        className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded mt-4"
+        onClick={handleDeleteSelected}
+      >
+        Delete Selected
+      </button>
     </div>
   );
 }
---
+++ src/components/Tablerow.jsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react'
+import { FaCheckSquare } from "react-icons/fa";
+import { MdDelete, MdEdit } from 'react-icons/md';
+
+
+
+
+const Tablerow = ({user,selectedRows,handleRowSelect,handleEditUser,handleDeleteSelected}) => {
+    const [editable,setEditable] = useState(false)
+
+  return (
+    <tr key={user.id} className={selectedRows.includes(user.id) ? 'bg-gray-200' : ''}>
+              <td className="px-4 py-3">
+                <input
+                  type="checkbox"
+                  onChange={() => handleRowSelect(user.id)}
+                  checked={selectedRows.includes(user.id)}
+                />
+              </td>
+              <td className="px-4 py-3">
+                <input
+                  type="text"
+                  value={user.name}
+                  className = {`outline-none p-1 ${editable && 'border '}`}
+                  onChange={(e) => handleEditUser(user.id, 'name', e.target.value)}
+                  readOnly={!editable}
+                 
+                />
+              </td>
+              <td className="px-4 py-3">
+                <input
+                  type="text"
+                  value={user.email}
+                  onChange={(e) => handleEditUser(user.id, 'email', e.target.value)}
+                />
+              </td>
+              <td className="px-4 py-3">{user.role}</td>
+              <td className="px-4 py-3">
+               { editable?(
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
+                  onClick={() => setEditable(!editable)}
+                >
+                  <FaCheckSquare />
+                </button>
+               ):(
+                <button
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
+                  onClick={() => {
+                    handleEditUser(user.id, 'email', user.email)
+                    setEditable(!editable)
+                  }}
+                >
+                  <MdEdit />
+                </button>
+               )
+                
+               }
+                <button
+                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                  onClick={() => handleDeleteSelected(user.id)}
+                >
+                  <MdDelete />
+                </button>
+              </td>
+            </tr>
+  )
+}
+
+export default Tablerow
